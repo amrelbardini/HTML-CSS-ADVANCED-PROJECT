a6796d3d7bec5fd4423ddd636709c983d762ca30
Amr Elbardini <49646105+amrelbardini@users.noreply.github.com>
2021-03-25 10:08:42 +0200

    added loadmore button

---
+++ JS/main.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+    
+   //load more button
+    let loadMore_btn=$("#load_more");
+    loadMore_btn.click(function(){
+        let hiddenItem=$(".item.hidden");
+        hiddenItem.toggle();
+    });
+});
+
+
+
+
